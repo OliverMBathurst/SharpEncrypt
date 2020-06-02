9e3697a4902a265bb0a2cb32f57feb6d0c5a8d85
OliverMBathurst <oliverbathurst12345@gmail.com>
2020-06-02 16:32:18 +0100

    Updated and improved FileGen project
    Updated and improved FileGen project

--- FileGen/FileGen.cs
+++ FileGen/FileGen.cs
@@ -1,70 +1,71 @@
 ﻿using ExtensionMethods.CharExtensionMethods;
 using ExtensionMethods.DateTimeExtensionMethods;
-using ExtensionMethods.EnumerableExtensionMethods;
 using System;
 using System.IO;
 using System.Linq;
 
 namespace FileGen
 {
-    class FileGen
+    internal class FileGen
     {
         static void Main(string[] args)
-            => Write(GetPath(args), GetSize(args), Random(args), Pause(args));
-
-        private static string GetPath(string[] args)
         {
-            var defPath = Path.GetPathRoot(Environment.CurrentDirectory);
-            var pathIndex = args.IndexOf("-path");
-            if(pathIndex != -1 && pathIndex + 1 < args.Length)
-                return args[pathIndex + 1];
+            var path = Environment.CurrentDirectory;
+            var size = -1L;
+            bool random = false, pause = true, postDelete = true;
 
-            return defPath;
+            if(args.Length == 1 && (args[0] == "-help" || args[0] == "-h"))
+            {
+                Usage();
+            }
+            else
+            {
+                for (var i = 0; i + 1 < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-path":
+                            path = args[i + 1];
+                            break;
+                        case "-size" when long.TryParse(args[i + 1], out var sizeResult):
+                            size = sizeResult;
+                            break;
+                        case "-random" when bool.TryParse(args[i + 1], out var randomResult):
+                            random = randomResult;
+                            break;
+                        case "-pause" when bool.TryParse(args[i + 1], out var pauseResult):
+                            pause = pauseResult;
+                            break;
+                        case "-postDelete" when bool.TryParse(args[i + 1], out var postResult):
+                            postDelete = postResult;
+                            break;
+                        default:
+                            throw new ArgumentException($"Invalid argument: {args[i]}\n{Usage()}");
+                    }
+                    i++;
+                }
+
+                Write(path, size, random, pause, postDelete);
+            }
         }
 
-        private static long GetSize(string[] args)
-        {
-            var sizeIndex = args.IndexOf("-size");
-            if (sizeIndex != -1 && sizeIndex + 1 < args.Length)
-                if (long.TryParse(args[sizeIndex + 1], out var longSize))
-                    return longSize;
-            return -1L;
-        }
+        private static string Usage() => "Usage: [-path] {true|false} [-size] {number} [-random] {true|false} [-pause] {true|false} [-postDelete] {true|false}";
 
-        private static bool Random(string[] args)
-        {
-            var randomIndex = args.IndexOf("-random");
-            if(randomIndex != -1 && randomIndex + 1 < args.Length)
-                if(bool.TryParse(args[randomIndex + 1], out var random))
-                    return random;
-            return false;
-        }
-
-        private static bool Pause(string[] args)
-        {
-            var pauseIndex = args.IndexOf("-pause");
-            if(pauseIndex != -1 && pauseIndex + 1 < args.Length)
-                if(bool.TryParse(args[pauseIndex + 1], out var pauseResult))
-                    return pauseResult;
-            return true;
-        }        
-
-        private static void Write(string path, long size, bool random, bool pause)
+        private static void Write(string path, long size, bool random, bool pause, bool postDelete)
         {
             if (!Directory.Exists(path))
                 throw new ArgumentException($"{path} is not a directory.");
 
             var drive = DriveInfo.GetDrives().First(x => x.Name[0].ToLower() == path[0].ToLower());
-            Console.WriteLine(drive);
 
             if (size == -1L)
                 size = drive.AvailableFreeSpace;
 
             var provider = new System.Security.Cryptography.RNGCryptoServiceProvider();
-            var genFilePath = $"{path}\'fileGen_{new DateTime().ToUnixTime()}.BIN";
+            var genFilePath = $"{path}\\fileGen_{DateTime.Now.ToUnixTime()}.BIN";
 
             Console.WriteLine($"Writing to: {genFilePath}");
-            using (var sw = new StreamWriter(File.Create(genFilePath)))
+            using (var sw = new BinaryWriter(File.Create(genFilePath)))
             {
                 var completed = false;
                 while (!completed)
@@ -84,12 +85,15 @@ namespace FileGen
 
                     if (size == 0L)
                         completed = true;
-                }                
+                }
             }
+
+            if (postDelete)
+                File.Delete(genFilePath);
 
             if (pause)
             {
-                Console.WriteLine("The file has been generated, press any key to continue...");
+                Console.WriteLine("The file has been generated, press enter to continue...");
                 Console.ReadLine();
             }                
         }
